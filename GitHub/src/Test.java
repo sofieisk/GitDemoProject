b857aef2a24ec927faa3f64ddc41dd699787353a
Sofie <sofieiskander@yahoo.com>
2019-10-08 12:33:54 -0400

    First commit

---
+++ GitHub/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if (hungry=="yes") {
+			System.out.println("eat");
+		}else {
+			System.out.println("dont eat");
+		}
+
+	}
+
+}
